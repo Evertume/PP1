8f6543ab56807b683e2760dc6f5d25b94a149ccb
Dias Kuspanov <117449684+Evertume@users.noreply.github.com>
2022-11-05 14:50:06 +0600

    Add files via upload

---
+++ assignment_6/R.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+string toUpper (string s){
+    for (int i=0; i< s.size(); i++)
+    s[i]= toupper(s[i]);
+return s;
+}
+int main(){
+string s;
+cin >> s;
+cout << toUpper(s);
+}
