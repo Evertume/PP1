6173ff67ef0771c4a4d85a99e4ba3ce8ce4b8e4c
Dias Kuspanov <117449684+Evertume@users.noreply.github.com>
2022-11-05 14:41:52 +0600

    Add files via upload

---
+++ assignment_6/P.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void positive(int n){
+    int a;
+    a = abs(n);
+    cout << a;
+}
+
+int main(){
+int n;
+cin >> n;
+positive(n);
+}
